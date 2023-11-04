d194896efb00392996f6c862ee7785dee4b643dc
JayKisanDas <jaykisandas@gmail.com>
2023-11-04 13:54:58 +0530

    Modified icons

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -50,7 +50,7 @@ const PhotoGallery = () => {
           Gallery
         </h6>
         <h1 className="font-secondary display-4 text-white">
-          👨🏻‍👩🏻‍👦🏻‍🧒🏻Our Photo Gallery🖼️
+          🤵🏻💖👰🏻Our Photo Gallery🖼️
         </h1>
         <i className="far fa-heart text-white"></i>
       </div>
